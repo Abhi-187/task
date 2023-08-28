ff9a195ed7444066a7ed67bb373fcf98e293569f
influx <71868156+Abhi-187@users.noreply.github.com>
2023-08-29 03:19:30 +0530

    comments

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
+// Define a function expression that takes a path array as input.
+function getExpression(path) {
+    const operators = { "plus": "+", "minus": "-", "into": "*", }
+
+    let ex = ""
+
+    path.forEach(element => {
+        const value = operators[element]
+        if (value === undefined) {
+            ex += element;
+        } else {
+            ex += value;
+        }
+    });
+
+    return ex;
+}
+
+// Array to store history
 const history = [];
 
+// Middleware to parse mathematical operations from URL
 app.get('/:operation/:values', (req, res, next) => {
     const { operation, values } = req.params;
     const numbers = values.split('/').map(Number);
@@ -39,16 +60,17 @@ app.get('/:operation/:values', (req, res, next) => {
     res.json({ question, answer });
 });
 
-
+// Route to get history
 app.get('/history', (req, res) => {
     res.json(history);
 });
 
+// Universal route for handling 404
 app.get('*', (req, res) => {
     res.status(404).send('404 Page Not Found');
 });
 
-const PORT = 3000;
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
